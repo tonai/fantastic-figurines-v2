95bcda88c095add247a16b3be53c54cb1d941d26
Tony CABAYE <tony.cabaye@smile.fr>
2024-07-18 12:18:55 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" >
+	<head>
+		<title>Fantastic figurines</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+		<meta name="author" content="Tonaï" />
+		<meta name="description" content="Site personnel réservé exclusivement à la peinture de figurines fantastiques." />
+		<meta name="keywords" content="figurines, fantastiques, peinture, tonaï, accueil" />
+		<meta name="reply-to" content="tonai59@hotmail.fr" />
+		<link rel="stylesheet" media="screen" type="text/css" title="Style" href="css/style.css" />
+	</head>
+	<body>
+		<div id="page">
+			<div id="header">
+			</div>
+			<div id="calendrier">
+			</div>
+			<div id="menu">
+				<ul>
+					<li><a href="index.php" id="menuAccueil"></a></li>
+					<li><a href="presentation.php" id="menuTutoriaux"></a></li>
+					<li><a href="galerie.php" id="menuGalerie"></a></li>
+					<li><a href="contact.php" id="menuHistorique"></a></li>
+					<li><a href="lien.php" id="menuLien"></a></li>
+				</ul>
+			</div>
+			<div id="corps">
+			</div>
+		</div>
+	</body>
+</html>
